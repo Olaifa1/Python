8ba1e02c490a45109a35cc4e5b81d748561d9c7c
unknown <olaifawaliu@rocketmail.com>
2019-12-10 13:09:02 +0100

    first commit

--- File_handling.py
+++ File_handling.py
@@ -1,2 +1,26 @@
 myfile = open('Test.txt')
 
+contents = myfile.read()
+print(contents)
+
+##   Reading a file
+fileObj = open('Test.txt', 'r')
+contents = fileObj.read()
+fileObj.close()
+
+
+##  Write a file
+
+##  fileObj = open('Test.txt)
+##  fileObj.write("I am in Cohort 2")
+##  fileObj.close()
+
+
+## Write a file using "WITH" keyword
+
+with open("cohort2/olaifa.txt", 'w+') as myfile:
+               myfile.write('I am a humble guy')
+               myfile.seek(0)
+               contents1 = myfile.read()
+               print(contents1)
+               
